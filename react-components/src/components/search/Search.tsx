621d694ca272e4b64e0ea22116ff478f541e9069
victoriazal <victoriazalomnova19@gmail.com>
2023-10-25 18:34:35 +0300

    feat: display characters

--- react-components/src/components/search/Search.tsx
+++ react-components/src/components/search/Search.tsx
@@ -2,11 +2,39 @@ import { Component } from 'react';
 import './Search.css';
 
 export default class Search extends Component {
+  state = { serachValue: localStorage.getItem('inputData') || '' };
+
+  handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchValue: event.target.value });
+    console.log(event.target.value);
+  };
+
+  componentDidMount() {
+    console.log('Mount');
+    fetch('https://swapi.dev/api/people')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    // this.setState(JSON.parse(localStorage.getItem("inputData")))
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<{}>,
+    prevState: Readonly<{}>,
+    snapshot?: any
+  ): void {
+    localStorage.setItem('inputData', JSON.stringify(this.state));
+  }
   render() {
     return (
       <>
         <div className="search">
-          <input className="search-input" placeholder="..."></input>
+          <input
+            onChange={this.handleSearchChange}
+            className="search-input"
+            placeholder="..."
+          ></input>
           <button type="submit" className="search-btn">
             Search
           </button>
--- react-components/src/components/searchResults/Results.tsx
+++ react-components/src/components/searchResults/Results.tsx
@@ -2,14 +2,42 @@ import { Component } from 'react';
 import './Results.css';
 
 export default class Results extends Component {
-  render() {
+  state = {
+    characters: [
+      {
+        name: '',
+        eye_color: '',
+        birth_year: '',
+      },
+    ],
+    loaded: false,
+  };
+  componentDidMount(): void {
     fetch('https://swapi.dev/api/people')
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        this.setState({ characters: data.results, loaded: true });
+      });
+  }
+  render() {
     return (
       <>
-        <div className="results">
-          <h1>Results</h1>
+        <div className="wrapper">
+          <div className="results">
+            {this.state.loaded ? (
+              this.state.characters.map((elem) => {
+                return (
+                  <div className="character">
+                    <h5>{elem.name}</h5>
+                    <span>Eye color:{elem.eye_color}</span>
+                    <span>Birth year:{elem.birth_year}</span>
+                  </div>
+                );
+              })
+            ) : (
+              <div className="loading">Loading...</div>
+            )}
+          </div>
         </div>
       </>
     );
